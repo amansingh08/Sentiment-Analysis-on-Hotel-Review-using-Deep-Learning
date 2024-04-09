a7c0564ff43ed54586c97d811b71db01d2b530f3
Aman Singh <amansingh2k6@gmail.com>
2024-04-09 16:54:20 +0530

    This is second commit

--- webscrap.py
+++ webscrap.py
@@ -1,3 +1,6 @@
 import psycopg2
 
-connection = psycopg2.connect(user = "",)
+connection = psycopg2.connect(user ="pgadmin4",
+pasword="admin",host="localhost",port="5050",database = "TestDataBase")
+
+cursor = connection.cursor()
